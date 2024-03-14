9650268c9ceb3527a73129b99eadce66ff3bd01e
ViNi <vini@ViNis-MacBook-Pro.local>
2024-03-15 00:07:24 +0530

    revert back to changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NotFound from './components/NotFound'
 import Home from './components/home/Home'
 import Footer from './components/Footer'
+import About from './components/about/About'
+import Feedback from './components/feedback/Feedback'
 
 function App() {
 
@@ -17,8 +19,8 @@ function App() {
           <Route path="/" element={<Home/>}></Route>
           <Route path="/home" element={<Home />} />
           <Route path="/work" element={<Home />} />
-          <Route path="/about" element={<Home />} />
-          <Route path="/feedback" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/feedback" element={<Feedback />} />
           <Route path="/contact" element={<Home />} />
           <Route path="*" element={<Home />} />
       </Routes>
